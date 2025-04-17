559282585b75b0d1a8666050b110a5b48e0f2c10
SSO <sso@d.co>
2025-04-17 13:19:21 +0200

    replaced buttonConfirmed state by ref, get rid of unnecessary function

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,19 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Player() {
-	const [playerName, setPlayerName] = useState();
-	const [playerNameConfirmed, setPlayerNameConfirmed] = useState(false);
+	const [playerName, setPlayerName] = useState('');
 
-	function handlePlayerNameChange(e) {
-		setPlayerName(e.target.value);
-	}
+	const refPlayerName = useRef();
 
 	function handleClick() {
-		setPlayerNameConfirmed((confirm) => !confirm);
+		setPlayerName(refPlayerName.current.value);
 	}
 
 	return (
 		<section id='player'>
-			<h2>
-				{playerNameConfirmed ? `Welcome ${playerName}!` : 'Please introduce your name!'}
-			</h2>
+			<h2> Welcome {playerName ? playerName : 'unknown entity'}</h2>
 			<p>
-				<input type='text' onChange={handlePlayerNameChange} />
+				<input ref={refPlayerName} type='text' />
 				<button onClick={handleClick}>Set Name</button>
 			</p>
 		</section>
