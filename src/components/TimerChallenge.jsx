1fc9dc3e6ea6ee8b315b96967f3046e6eec706bb
SSO <sso@d.co>
2025-04-18 15:23:27 +0200

    added ResulModal component to show modal dialog, used old version of ref forwarding

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,38 +1,46 @@
 import { useRef, useState } from 'react';
 
-function TimerChallenge({ title, targetTime }) {
+import ResultModal from './ResultModal.jsx';
+
+// let timer;
+
+export default function TimerChallenge({ title, targetTime }) {
+	const timer = useRef();
+	const dialog = useRef();
+
 	const [timerStarted, setTimerStarted] = useState(false);
 	const [timerExpired, setTimerExpired] = useState(false);
 
-	const timer = useRef();
-
 	function handleStart() {
-		setTimerStarted(true);
-
 		timer.current = setTimeout(() => {
 			setTimerExpired(true);
+			dialog.current.showModal();
 		}, targetTime * 1000);
+
+		setTimerStarted(true);
 	}
+
 	function handleStop() {
 		clearTimeout(timer.current);
 	}
+
 	return (
-		<section className='challenge'>
-			<h2>{title}</h2>
-			{timerExpired && <p>You lost</p>}
-			<p className='challenge-time'>
-				{targetTime} second{targetTime > 1 ? 's' : ''}
-			</p>
-			<p>
-				<button onClick={timerStarted ? handleStop : handleStart}>
-					{timerStarted ? 'Stop' : 'Start'} Challenge
-				</button>
-			</p>
-			<p className={timerStarted ? 'active' : undefined}>
-				{timerStarted ? 'Time is running...' : 'Timer inactive'}
-			</p>
-		</section>
+		<>
+			<ResultModal ref={dialog} targetTime={targetTime} result='lost' />
+			<section className='challenge'>
+				<h2>{title}</h2>
+				<p className='challenge-time'>
+					{targetTime} second{targetTime > 1 ? 's' : ''}
+				</p>
+				<p>
+					<button onClick={timerStarted ? handleStop : handleStart}>
+						{timerStarted ? 'Stop' : 'Start'} Challenge
+					</button>
+				</p>
+				<p className={timerStarted ? 'active' : undefined}>
+					{timerStarted ? 'Time is running...' : 'Timer inactive'}
+				</p>
+			</section>
+		</>
 	);
 }
-
-export default TimerChallenge;
